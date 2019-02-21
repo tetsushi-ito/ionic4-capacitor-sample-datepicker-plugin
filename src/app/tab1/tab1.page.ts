9769fadcb87ee67cb31aff08aad9d14826d46f76
tetsushi <tetsushi0501@gmail.com>
2019-02-21 18:34:50 +0900

    add DatePicker Plugin

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { DatePicker } from 'capacitor-datepicker';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+  date;
+
+  async showDatePicker() {
+
+    const picker = new DatePicker();
+
+    const current = new Date();
+
+    const response = await picker.show({
+      mode: 'date',
+      date: current.toISOString(),
+      theme: 'AppDialogTheme',
+      title: '日付を選択',
+      okText: 'OK',
+      cancelText: 'キャンセル',
+      is24Hours: false,
+    });
+
+    this.date = response.value;
+  }
+}
